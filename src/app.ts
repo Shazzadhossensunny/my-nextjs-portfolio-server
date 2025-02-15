8049c8a6e83901e3dc9a0873b5c4498102f88b44
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-02-16 01:53:31 +0600

    some bug fix

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,13 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+// app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(
+  cors({
+    origin: ['https://my-nextjs-portfolio-murex.vercel.app'],
+    credentials: true,
+  })
+);
 
 // application routes
 app.use('/api', router);
